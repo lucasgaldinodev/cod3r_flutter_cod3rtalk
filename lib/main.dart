e917c109449863b7c05c75c579ac4baff4f57d7b
Lucas Galdino <lucasgaldino7@gmail.com>
2022-02-21 11:16:40 -0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -4,8 +4,26 @@ import 'tela2.dart';
 
 void main() => runApp(const MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  int _indiceAtual = 0;
+
+  final List _telas = [
+    const Tela1(),
+    const Tela2(),
+  ];
+
+  void quandoClicar(int indice) {
+    setState(() {
+      _indiceAtual = indice;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -15,7 +33,24 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.purple,
         brightness: Brightness.dark,
       ),
-      home: const Tela2(),
+      home: Scaffold(
+        body: _telas[_indiceAtual],
+        bottomNavigationBar: BottomNavigationBar(
+          selectedItemColor: Colors.deepPurple[400],
+          currentIndex: _indiceAtual,
+          onTap: quandoClicar,
+          items: const [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.looks_one),
+              label: 'Página 1',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.looks_two),
+              label: 'Página 2',
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
